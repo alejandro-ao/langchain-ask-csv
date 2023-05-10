f84b9fb0facad6958d5edc96f5b7a21bcf8a21b2
Alejandro <aavila0707@gmail.com>
2023-05-10 15:23:44 +0200

    fix: remove f string, parsing problems

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from langchain.llms import OpenAI
 from dotenv import load_dotenv
 import os
 import streamlit as st
-import seaborn as sns
 
 
 def main():
@@ -23,13 +22,13 @@ def main():
     if csv_file is not None:
 
         agent = create_csv_agent(
-            OpenAI(temperature=0, model_name="gpt-3.5-turbo"), csv_file, verbose=True )
+            OpenAI(temperature=0), csv_file, verbose=True)
 
         user_question = st.text_input("Ask a question about your CSV: ")
-        
+
         if user_question is not None and user_question != "":
             with st.spinner(text="In progress..."):
-                st.write(agent.run(f"{user_question}"))
+                st.write(agent.run(user_question))
 
 
 if __name__ == "__main__":
